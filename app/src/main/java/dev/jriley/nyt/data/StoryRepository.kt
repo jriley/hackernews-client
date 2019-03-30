6b01dd3f72afa2db339d9edfcc94abf117274296
jason riley <jriley@jasons-MacBook-Pro.local>
2019-03-29 21:13:17 -0500

    Set up repository to handle the api. jriley

--- app/src/main/java/dev/jriley/nyt/data/StoryRepository.kt
+++ app/src/main/java/dev/jriley/nyt/data/StoryRepository.kt
@@ -3,20 +3,38 @@ package dev.jriley.nyt.data
 import dev.jriley.nyt.service.HackerNewsService
 import dev.jriley.nyt.service.KotlinServices
 import dev.jriley.nyt.service.Story
-import io.reactivex.Maybe
 import io.reactivex.Scheduler
 import io.reactivex.Single
 import io.reactivex.schedulers.Schedulers
 
 
 class StoryRepository(
+    private val storyData: StoryData,
     private val hackerNewsService: HackerNewsService = KotlinServices.hackerNewsService,
     private val scheduler: Scheduler = Schedulers.io()
 ) {
     fun loadNewIds(): Single<List<Long>> = hackerNewsService.new.subscribeOn(scheduler)
     fun loadTopIds(): Single<List<Long>> = hackerNewsService.top.subscribeOn(scheduler)
     fun loadBestIds(): Single<List<Long>> = hackerNewsService.best.subscribeOn(scheduler)
-    fun fetchStory(id: String): Maybe<Story> = hackerNewsService.getStory(id)
-        .flatMapMaybe { response -> response.body()?.let { Maybe.just(it) } ?: Maybe.empty() }
+    fun fetchStory(id: String): Single<Story> = hackerNewsService.getStory(id)
+        .map { response -> response.body()?.let { it } ?: Story(-1) }
         .subscribeOn(scheduler)
+
+    fun fetchNewStories(): Single<List<Long>> {
+        return hackerNewsService.new
+            .toObservable()
+            .flatMapIterable {  list -> list}
+            .map { id -> id.toString() }
+            .map { id -> fetchStory(id) }
+            .flatMapSingle { single -> single }
+            .map { if(it.id > 0) storyData.insert(story = it); it }
+            .map { it.id }
+            .filter { it > 0 }
+            .toList()
+    }
+}
+
+interface StoryData {
+    // The returned long is database row id, NOT the story id.
+    fun insert(story: Story): Single<Long>
 }
--- app/src/test/java/dev/jriley/nyt/data/StoryRepositoryTest.kt
+++ app/src/test/java/dev/jriley/nyt/data/StoryRepositoryTest.kt
@@ -1,11 +1,11 @@
 package dev.jriley.nyt.data
 
-import com.nhaarman.mockito_kotlin.mock
-import com.nhaarman.mockito_kotlin.whenever
+import com.nhaarman.mockito_kotlin.*
 import dev.jriley.nyt.service.HackerNewsService
 import dev.jriley.nyt.service.Story
 import io.reactivex.Single
 import io.reactivex.schedulers.TestScheduler
+import org.junit.Before
 import org.junit.Test
 import retrofit2.Response
 import kotlin.random.Random
@@ -13,14 +13,19 @@ import kotlin.random.Random
 class StoryRepositoryTest {
 
     private lateinit var testObject: StoryRepository
+    private val storyData: StoryData = mock()
     private val hackerNewsService: HackerNewsService = mock()
     private val testScheduler: TestScheduler = TestScheduler()
 
+    @Before
+    fun setUp() {
+        testObject = StoryRepository(storyData, hackerNewsService, testScheduler)
+    }
+
     @Test
     fun `call new story services on io thread`() {
-        val expectedIds = (0..Random.nextLong(1,20)).toList()
+        val expectedIds = (0..Random.nextLong(1, 20)).toList()
         whenever(hackerNewsService.new).thenReturn(Single.just(expectedIds))
-        testObject = StoryRepository(hackerNewsService, testScheduler)
 
         val testObserver = testObject.loadNewIds().test()
 
@@ -41,9 +46,8 @@ class StoryRepositoryTest {
 
     @Test
     fun `call top story services on io thread`() {
-        val expectedIds = (0..Random.nextLong(1,20)).toList()
+        val expectedIds = (0..Random.nextLong(1, 20)).toList()
         whenever(hackerNewsService.top).thenReturn(Single.just(expectedIds))
-        testObject = StoryRepository(hackerNewsService, testScheduler)
 
         val testObserver = testObject.loadTopIds().test()
 
@@ -64,9 +68,8 @@ class StoryRepositoryTest {
 
     @Test
     fun `call best story services on io thread`() {
-        val expectedIds = (0..Random.nextLong(1,20)).toList()
+        val expectedIds = (0..Random.nextLong(1, 20)).toList()
         whenever(hackerNewsService.best).thenReturn(Single.just(expectedIds))
-        testObject = StoryRepository(hackerNewsService, testScheduler)
 
         val testObserver = testObject.loadBestIds().test()
 
@@ -91,7 +94,6 @@ class StoryRepositoryTest {
         val story = Story(expectedId)
         val response = Response.success(200, story)
         whenever(hackerNewsService.getStory("$expectedId")).thenReturn(Single.just(response))
-        testObject = StoryRepository(hackerNewsService, testScheduler)
 
         val testObserver = testObject.fetchStory("$expectedId").test()
 
@@ -114,9 +116,8 @@ class StoryRepositoryTest {
     @Test
     fun `don't trust the service it returns null body from time to time`() {
         val expectedId = Random.nextLong()
-        val response: Response<Story> = Response.success( null)
+        val response: Response<Story> = Response.success(null)
         whenever(hackerNewsService.getStory("$expectedId")).thenReturn(Single.just(response))
-        testObject = StoryRepository(hackerNewsService, testScheduler)
 
         val testObserver = testObject.fetchStory("$expectedId").test()
 
@@ -131,7 +132,49 @@ class StoryRepositoryTest {
         testObserver.apply {
             assertNoErrors()
             assertComplete()
+            assertValue(Story(-1))
+        }
+    }
+
+    @Test
+    fun `tie it together and insert into the database and return successful ids`() {
+        val givenId1 = 2L
+        val givenId2 = 3L
+        val givenId4 = 4L
+        val expectedIds = listOf(givenId1, givenId2)
+        val wireIds = listOf(givenId4, givenId1, givenId2)
+        val story1 = Story(givenId1)
+        val story2 = Story(givenId2)
+        val response1 = Response.success(200, story1)
+        val response2 = Response.success(200, story2)
+        val responseNull: Response<Story> = Response.success(null)
+        whenever(hackerNewsService.new).thenReturn(Single.just(wireIds))
+        whenever(storyData.insert(story1)).thenReturn(Single.just(Long.MAX_VALUE))
+        whenever(storyData.insert(story2)).thenReturn(Single.just(Long.MAX_VALUE))
+        whenever(hackerNewsService.getStory("$givenId1")).thenReturn(Single.just(response1))
+        whenever(hackerNewsService.getStory("$givenId2")).thenReturn(Single.just(response2))
+        whenever(hackerNewsService.getStory("$givenId4")).thenReturn(Single.just(responseNull))
+
+        val testObserver = testObject.fetchNewStories().test()
+
+        testObserver.apply {
+            assertNoErrors()
+            assertNotComplete()
             assertNoValues()
         }
+
+        testScheduler.triggerActions()
+
+        testObserver.apply {
+            assertNoErrors()
+            assertComplete()
+            assertValue(expectedIds)
+        }
+
+        verify(storyData).insert(story1)
+        verify(storyData).insert(story2)
+        verify(hackerNewsService).getStory("$givenId1")
+        verify(hackerNewsService).getStory("$givenId2")
+        verify(hackerNewsService).getStory("$givenId4")
     }
 }
