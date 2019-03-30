425a1eb45b0c4678ce1469ab5a01b13ec468171e
jason riley <jriley@jasons-MacBook-Pro.local>
2019-03-30 17:36:56 -0500

    refactored into single fun. jriley

--- app/src/main/java/dev/jriley/nyt/data/StoryRepository.kt
+++ app/src/main/java/dev/jriley/nyt/data/StoryRepository.kt
@@ -27,47 +27,23 @@ class StoryRepository(
     fun loadBestIds(): Single<List<Long>> = hackerNewsService.best.subscribeOn(scheduler)
     fun fetchStory(id: String): Single<Response<Story>> = hackerNewsService.getStory(id).subscribeOn(scheduler)
 
-    fun fetchNewStories(): Single<List<Long>> {
-        return loadNewIds()
-            .map { id -> Timber.tag("@@@@").i("Original  size: ${id.size}"); id.filter { !storyData.isLoaded(it) } }
-            .toObservable()
-            .flatMapIterable { filteredList -> Timber.tag("@").i("Filtered size:${filteredList.size}"); filteredList }
-            .map { filteredId -> hackerNewsService.getStory(filteredId.toString()) }
-            .flatMapSingle { single -> single }
-            .map { story -> story.body()?.run { storyData.insert(Story(this, StoryTypes.NEW)) }?: Single.just(-1L) }
-            .flatMapSingle { single -> single }
-            .filter { it > 0 }
-            .toList()
-            .subscribeOn(scheduler)
-    }
+    fun fetchNewStories(): Single<List<Long>> = fetchAndInsert(loadNewIds(), StoryTypes.NEW)
 
-    fun fetchTopStories(): Single<List<Long>> {
-        return loadTopIds()
-            .map { id -> Timber.tag("@@@@").i("Original  size: ${id.size}"); id.filter { !storyData.isLoaded(it) } }
-            .toObservable()
-            .flatMapIterable { filteredList -> Timber.tag("@").i("Filtered size:${filteredList.size}"); filteredList }
-            .map { filteredId -> hackerNewsService.getStory(filteredId.toString()) }
-            .flatMapSingle { single -> single }
-            .map { story -> story.body()?.run { storyData.insert(Story(this, StoryTypes.TOP)) }?: Single.just(-1L) }
-            .flatMapSingle { single -> single }
-            .filter { it > 0 }
-            .toList()
-            .subscribeOn(scheduler)
-    }
+    fun fetchTopStories(): Single<List<Long>> = fetchAndInsert(loadTopIds(), StoryTypes.TOP)
 
-    fun fetchBestStories(): Single<List<Long>> {
-        return loadBestIds()
-            .map { id -> Timber.tag("@@@@").i("Original  size: ${id.size}"); id.filter { !storyData.isLoaded(it) } }
+    fun fetchBestStories(): Single<List<Long>> = fetchAndInsert(loadBestIds(), StoryTypes.BEST)
+
+    private fun fetchAndInsert(api: Single<List<Long>>, filter: StoryTypes) : Single<List<Long>> =
+        api.map { id -> Timber.tag("@@@@").i("Original  size: ${id.size}"); id.filter { !storyData.isLoaded(it) } }
             .toObservable()
             .flatMapIterable { filteredList -> Timber.tag("@").i("Filtered size:${filteredList.size}"); filteredList }
             .map { filteredId -> hackerNewsService.getStory(filteredId.toString()) }
             .flatMapSingle { single -> single }
-            .map { story -> story.body()?.run { storyData.insert(Story(this, StoryTypes.BEST)) }?: Single.just(-1L) }
+            .map { story -> story.body()?.run { storyData.insert(Story(this, filter)) }?: Single.just(-1L) }
             .flatMapSingle { single -> single }
             .filter { it > 0 }
             .toList()
             .subscribeOn(scheduler)
-    }
 }
 
 interface StoryData {
